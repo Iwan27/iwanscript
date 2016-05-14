61f2354a2f27ed48ff31cc5e61fe4095b2d263d6
Iwan27 <Iwan27@users.noreply.github.com>
2016-05-14 23:41:21 +0700

    Update disqus.js

--- disqus.js
+++ disqus.js
@@ -1,21 +1,25 @@
-                var disqus_shortname = &quot;iwanberbagicom&quot;;
-                var disqus_blogger_current_url = &quot;<data:blog.canonicalUrl/>&quot;;
-                if (!disqus_blogger_current_url.length) {
-                    disqus_blogger_current_url = &quot;<data:blog.url/>&quot;;
-                }
-                var disqus_blogger_homepage_url = &quot;<data:blog.homepageUrl/>&quot;;
-                var disqus_blogger_canonical_homepage_url = &quot;<data:blog.canonicalHomepageUrl/>&quot;;
-             (function() {
-                        var bloggerjs = document.createElement(&quot;script&quot;);
-                        bloggerjs.type = &quot;text/javascript&quot;;
-                        bloggerjs.async = true;
-                        bloggerjs.src = &quot;//&quot;+disqus_shortname+&quot;.disqus.com/blogger_item.js&quot;;
-                        (document.getElementsByTagName(&quot;head&quot;)[0] || document.getElementsByTagName(&quot;body&quot;)[0]).appendChild(bloggerjs);
-                    })();
-                (function() {
-                    var bloggerjs = document.createElement(&quot;script&quot;);
-                    bloggerjs.type = &quot;text/javascript&quot;;
-                    bloggerjs.async = true;
-                    bloggerjs.src = &quot;//&quot;+disqus_shortname+&quot;.disqus.com/blogger_index.js&quot;;
-                    (document.getElementsByTagName(&quot;head&quot;)[0] || document.getElementsByTagName(&quot;body&quot;)[0]).appendChild(bloggerjs);
-                })();
+ var disqus_shortname="iwanberbagicom";
+var disqus_url = disqus_blogger_current_url;
+
+(function () {
+    "use strict";
+    var get_comment_block = function () {
+        var block = document.getElementById('comments');
+        if (!block) {
+            block = document.getElementById('disqus-blogger-comment-block');
+        }
+        return block;
+    };
+    var comment_block = get_comment_block();
+    if (!!comment_block) {
+        var disqus_div = document.createElement('div');
+        disqus_div.id = 'disqus_thread';
+        comment_block.innerHTML = '';
+        comment_block.appendChild(disqus_div);
+        comment_block.style.display = 'block';
+        var dsq = document.createElement('script');
+        dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.body).appendChild(dsq);
+    }
+})();
